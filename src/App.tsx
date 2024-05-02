40463d6a016f6bceb3be3f6490818c858367566d
Vishal Yadav <vishalyxo@gmail.com>
2024-05-02 18:17:10 +0530

    y

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import "./App.css";
-// import Bookmarks from "./Components/Bookmarks/Bookmarks";
+import Bookmarks from "./Components/Bookmarks/Bookmarks";
 import Greeting from "./Components/Greeting/Greeting";
 import Time from "./Components/Time/Time";
 import Todo from "./Components/Todo/Todo";
-// import Weather from "./Components/Weather/Weather";
-// import Crypto from "./Components/Crypto/Crypto";
+import Weather from "./Components/Weather/Weather";
+import Crypto from "./Components/Crypto/Crypto";
 
 function App() {
   return (
     <div className="app-wrapper bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="App">
-        {/* <div className="item1">
+        <div className="item1">
           <Bookmarks />
-        </div> */}
+        </div>
         <div className="item2">
           <Time />
         </div>
         <div className="item3">
           <Greeting />
         </div>
-        {/* <div className="item4">
+        <div className="item4">
           <Weather />
-        </div> */}
+        </div>
         <div className="item5">
           <Todo />
         </div>
-        {/* <div className="item6">
+        <div className="item6">
           <Crypto />
-        </div> */}
+        </div>
       </div>
     </div>
   );
