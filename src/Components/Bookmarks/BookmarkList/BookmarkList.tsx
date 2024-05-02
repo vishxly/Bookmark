9e1ae63e3d07db3778a0dcb3699d9f9e34ce89e1
Vishal Yadav <vishalyxo@gmail.com>
2024-05-02 17:08:57 +0530

    Second Commit

--- src/Components/Bookmarks/BookmarkList/BookmarkList.tsx
+++ src/Components/Bookmarks/BookmarkList/BookmarkList.tsx
@@ -30,13 +30,13 @@ const BookmarkList: React.FC<BookmarkListProps> = ({
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="2"
+            strokeWidth="2"
             stroke="currentColor"
             className="w-5 h-5"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
             ></path>
           </svg>
@@ -50,7 +50,6 @@ const BookmarkList: React.FC<BookmarkListProps> = ({
           ))}
         </ul>
       </div>
-      
     </section>
   );
 };
